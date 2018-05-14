733ec7aa50bbb26c876f0b311be7cd784bc9d185
chris.stadler <stadler.christian@web.de>
2018-05-14 20:17:28 +0200

    replace brightness array calculation with the result.

--- src/main.ino
+++ src/main.ino
@@ -1,58 +1,38 @@
 #include <Arduino.h>
-#include "Fader.h"
+
 // for ATTiny85
 // on button pressed fade led in stay for n-sec. and fade out
-Fader fader = Fader();
+
+//int brightness[] = { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16, 18, 21, 23, 26, 30, 33, 37, 42, 47, 53, 59, 66, 74, 83, 93, 104, 116, 130, 145, 162, 181, 203, 227, 255 };
+int brightness[] = { 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 4, 5, 6, 7, 9, 11, 13, 15, 18, 22, 26, 31, 37, 44, 53, 62, 74, 87, 104, 123, 145, 172, 203, 240, 255 };
+//int brightness[] = { 0, 0, 0, 0, 1, 2, 2, 3, 4, 6, 8, 10, 13, 16, 21, 26, 33, 42, 53, 66, 83, 104, 130, 162, 203, 255 };
+int count = (sizeof(brightness)/sizeof(*brightness));
 
 const int triggerPin =0;//LED
 const int ledPin = 1;  // LED
 const int inputPin = 2;// button or PIR-Sensor
 const int potiPin = 3;
 
-const int maxarr= 64;
-int brightnessArrayMax = 0;
-float brightnessArray[maxarr];
-
 int processing = -1;
 int maxProcessingCount = 10;
 
-void InitBrightness(float *arr, int *max)
-{
-  const int pwmIntervals = 100;
-  // Calculate the R variable (only needs to be done once at setup)
-  float R = (pwmIntervals * log10(2))/(log10(255));
-
-  // ramp up
-  int i=0;
-  for (int interval = 0; interval <= pwmIntervals; interval++)
-  {
-      // Calculate the required PWM value for this interval step
-      int brightness = pow (2, (interval / R)) - 1;
-      if (brightness < 2) continue;
-      // Set the LED output to the calculated brightness
-      arr[i++] = brightness;
-  }
-  for (int j = i, k=0; j<2*i;j++,k++) arr[j] =  arr[i-k];
-
-  *max = i*2;
-}
 void FadeIn(int delayMsec)
 {
-  for (size_t i = 0; i < brightnessArrayMax/2; i++) {
-       analogWrite(ledPin,  brightnessArray[i]);
+  for (size_t i = 0; i < brightness; i++) {
+       analogWrite(ledPin,  brightness[i]);
        delay(delayMsec);
      }
 }
 void FadeOut(int delayMsec)
 {
-  for (size_t i = brightnessArrayMax/2+1; i < brightnessArrayMax; i++) {
-       analogWrite(ledPin,  brightnessArray[i]);
+  for (size_t i = 0; i < brightness; i++) {
+       analogWrite(ledPin,  brightness[count-1-i]);
        delay(delayMsec);
      }
    analogWrite(ledPin, 0);
 }
 
-boolean IsButtonPressed()
+boolean IsTriggerSet()
 {
   if (digitalRead(inputPin) == HIGH) {
     // turn LED on:
@@ -64,9 +44,6 @@ boolean IsButtonPressed()
 }
 
 void setup() {
-  //Serial.begin(9600);
-  InitBrightness(brightnessArray, &brightnessArrayMax);
-
   pinMode(inputPin, INPUT);
   pinMode(potiPin, INPUT);
   processing = -1;
@@ -74,12 +51,12 @@ void setup() {
 }
 
 void loop() {
-//  Serial.print(IsButtonPressed());
+//  Serial.print(IsTriggerSet());
 //  Serial.print("\t");
 //  Serial.println(maxProcessingCount);
   maxProcessingCount = map(analogRead(potiPin), 0,1023, 0,50);
 
-  if (IsButtonPressed())
+  if (IsTriggerSet())
   {
     if (processing == -1)
     {
